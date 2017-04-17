41e0bde6d0a37791a892bbbfc44ea2d95c732432
ilgor <ilgorbek@gmail.com>
2017-04-17 16:52:53 -0500

    BST functionality added, Search and Remove are broken for now

--- DataStructures/BinarySearchTree/BST.py
+++ DataStructures/BinarySearchTree/BST.py
@@ -1,3 +1,73 @@
+class Node():
+    def __init__(self, data=None, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.data = data
+
+
 class BST():
     def __init__(self):
-        pass
+        self.size = 0
+        self.root = None
+
+    def insert(self, data):
+        self.root = self.insert_helper(self.root, data)
+        self.size += 1
+
+    def insert_helper(self, rootNode, data):
+        if rootNode == None:
+             return Node(data)
+        if data < rootNode.data:
+            rootNode.left = self.insert_helper(rootNode.left, data)
+        else:
+            rootNode.right = self.insert_helper(rootNode.right, data)
+        return rootNode
+
+    def search(self, data):
+        return self.search_helper(self.root, data)
+
+    def search_helper(self, rootNode, data):
+        if rootNode == None or rootNode.data == data:
+            return rootNode
+        if data < rootNode.data:
+            self.search_helper(rootNode.left, data)
+        else:
+            self.search_helper(rootNode.right, data)
+
+    def remove(self, data):
+        node = self.search(data)
+        if node.left == None and node.right == None:
+
+
+    def pre_order(self):
+        arr = []
+        self.pre_order_helper(self.root, arr)
+        return arr
+
+    def pre_order_helper(self, rootNode, arr):
+        if rootNode != None:
+            arr.append(rootNode.data)# print(rootNode.data)
+            self.pre_order_helper(rootNode.left, arr)
+            self.pre_order_helper(rootNode.right, arr)
+
+    def post_order(self):
+        arr = []
+        self.post_order_helper(self.root, arr)
+        return arr
+
+    def post_order_helper(self, rootNode, arr):
+        if rootNode != None:
+            self.post_order_helper(rootNode.left, arr)
+            self.post_order_helper(rootNode.right, arr)
+            arr.append(rootNode.data)  # print(rootNode.data)
+
+    def in_order(self):
+        arr = []
+        self.post_order_helper(self.root, arr)
+        return arr
+
+    def in_order_helper(self, rootNode, arr):
+        if rootNode != None:
+            self.in_order_helper(rootNode.left, arr)
+            arr.append(rootNode.data)  # print(rootNode.data)
+            self.in_order_helper(rootNode.right, arr)
--- DataStructures/BinarySearchTree/BSTTest.py
+++ DataStructures/BinarySearchTree/BSTTest.py
@@ -5,4 +5,76 @@ from BST import BST
 
 class BSTTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.bst = BST()
+
+    def show(self, msg=None):
+        print(inspect.stack()[1][3], msg)
+
+    def test_insert_root(self):
+        self.bst.insert(10)
+        self.assertEqual(1, self.bst.size)
+        self.assertEqual(10, self.bst.root.data)
+        self.show([('size', self.bst.size), ('root value', self.bst.root.data)])
+
+    def test_insert_left(self):
+        self.bst.insert(10)
+        self.bst.insert(5)
+        self.assertEqual(2, self.bst.size)
+        self.assertEqual(10, self.bst.root.data)
+        self.assertEqual(5, self.bst.root.left.data)
+        self.show([('size', self.bst.size), ('root value', self.bst.root.data), ('left child', self.bst.root.left.data)])
+
+    def test_insert_right(self):
+        self.bst.insert(10)
+        self.bst.insert(19)
+        self.assertEqual(2, self.bst.size)
+        self.assertEqual(10, self.bst.root.data)
+        self.assertEqual(19, self.bst.root.right.data)
+        self.show([('size', self.bst.size), ('root value', self.bst.root.data), ('right child', self.bst.root.right.data)])
+
+    def test_search_existing_item(self):
+        self.populate()
+        node = self.bst.search(6)
+        self.show(self.bst.in_order())
+        self.assertIsNotNone(node)
+        self.assertEqual(6, node.data)
+        # self.show('Found!')
+
+    def test_search_none_existing_item(self):
+        self.populate()
+        node = self.bst.search(100)
+        self.assertIsNone(node)
+        self.show('Not Found!')
+
+    def test_remove_existing_item(self):
+        self.populate()
+        size = self.bst.size
+        expected_size = size - 1
+        self.bst.remove(5)
+        result = self.bst.search(5)
+        self.assertIsNotNone(result)
+        self.assertEqual(expected_size, size)
+
+    def test_remove_none_existing_item(self):
+        self.populate()
+        with self.assertRaises(Exception):
+            self.bst.remove(5)
+
+    def test_pre_order(self):
+        self.populate()
+        result = self.bst.pre_order()
+        self.show(result)
+
+    def test_post_order(self):
+        self.populate()
+        result = self.bst.post_order()
+        self.show(result)
+
+    def test_in_order(self):
+        self.populate()
+        result = self.bst.in_order()
+        self.show(result)
+
+    def populate(self):
+        for i in [10,5,19,1,6,17]:
+            self.bst.insert(i)
