695959318fcda900d873e31226c3bc8c5c502b79
ilgor <ilgorbek@gmail.com>
2017-04-20 16:19:56 -0500

    cleanup

--- DataStructures/BinarySearchTree/BST.py
+++ DataStructures/BinarySearchTree/BST.py
@@ -1,9 +1,9 @@
 class Node():
-    def __init__(self, parent=None, data=None):
+    def __init__(self, data=None, parent=None):
         self.parent = parent
         self.data = data
-        self.leftChild = None
-        self.rightChild = None
+        self.left_child = None
+        self.right_child = None
 
 
 class BST():
@@ -11,72 +11,73 @@ class BST():
         self.size = 0
         self.root = None
 
+    def search(self, data):
+        parent = None
+        current = self.root
+        while current and current.data != data:
+            parent = current
+            if data < current.data:
+                current = current.left_child
+            else:
+                current = current.right_child
+        return current, parent
+
     def insert(self, data):
-        self.root = self.insert_helper(self.root, data)
+        parent = None
+        current = self.root
+        while current:
+            parent = current
+            if data < current.data:
+                current = current.left_child
+            else:
+                current = current.right_child
+        new_node = Node(data, parent)
+        if parent is None:
+            self.root = new_node
+        elif new_node.data < parent.data:
+            parent.left_child = new_node
+        else:
+            parent.right_child = new_node
         self.size += 1
 
-    def insert_helper(self, rootNode, data):
-        if rootNode == None:
-            return Node(data)
-        if data < rootNode.data:
-            rootNode.left = self.insert_helper(rootNode.left, data)
-        else:
-            rootNode.right = self.insert_helper(rootNode.right, data)
-        return rootNode
+    def find_min(self, node):
+        parent = None
+        current = node
+        while current:
+            parent = current
+            current = current.left_child
+        return parent
 
-    def search(self, data):
-        return self.search_helper(self.root, data)
-
-    def search_helper(self, rootNode, data):
-        if rootNode == None or rootNode.data == data:
-            return rootNode
-        if data < rootNode.data:
-            self.search_helper(rootNode.left, data)
-        else:
-            self.search_helper(rootNode.right, data)
-
-    def remove(self, data):
-        node = self.search(data)
-        if node.left:
-            node.data = node.left.data
-            node.left = None
-        elif node.right:
-            node.data = node.right.data
-            node.right = None
-
-        if node.left == None and node.right == None:
-            pass
-
-
-    def pre_order(self):
-        arr = []
-        self.pre_order_helper(self.root, arr)
-        return arr
-
-    def pre_order_helper(self, rootNode, arr):
-        if rootNode != None:
-            arr.append(rootNode.data)  # print(rootNode.data)
-            self.pre_order_helper(rootNode.left, arr)
-            self.pre_order_helper(rootNode.right, arr)
-
-    def post_order(self):
-        arr = []
-        self.post_order_helper(self.root, arr)
-        return arr
-
-    def post_order_helper(self, rootNode, arr):
-        if rootNode != None:
-            self.post_order_helper(rootNode.left, arr)
-            self.post_order_helper(rootNode.right, arr)
-            arr.append(rootNode.data)  # print(rootNode.data)
+    def find_max(self, node):
+        parent = None
+        current = node
+        while current:
+            parent = current
+            current = current.right_child
+        return parent
 
     def in_order(self):
         arr = []
-        self.post_order_helper(self.root, arr)
+        return self.in_order_helper(self.root, arr)
+
+    def in_order_helper(self, node, arr):
+        if node:
+            self.in_order_helper(node.left_child, arr)
+            arr.append(node.data)
+            self.in_order_helper(node.right_child, arr)
         return arr
 
-    def in_order_helper(self, rootNode, arr):
-        if rootNode != None:
-            self.in_order_helper(rootNode.left, arr)
-            arr.append(rootNode.data)  # print(rootNode.data)
-            self.in_order_helper(rootNode.right, arr)
+    def remove(self, data):
+        current, parent = self.search(data)
+        if current is None:
+            raise Exception(str(data)+' doesnt exit')
+        if current.left_child is None and current.right_child is None:
+            if parent.left_child and parent.left_child.data == data:
+                parent.left_child = None
+            else:
+                parent.right_child = None
+        elif current.left_child and current.right_child is None:
+            parent.left_child = current.left_child
+            current = None
+
+        self.size -= 1
--- DataStructures/BinarySearchTree/BSTTest.py
+++ DataStructures/BinarySearchTree/BSTTest.py
@@ -6,77 +6,52 @@ from BST import BST
 class BSTTest(unittest.TestCase):
     def setUp(self):
         self.bst = BST()
+        self.arr = [3,1,2,0]
+        for item in self.arr:
+            self.bst.insert(item)
 
     def show(self, msg=None):
         print(inspect.stack()[1][3], msg)
 
-    def test_insert_root(self):
-        self.bst.insert(10)
-        self.assertEqual(1, self.bst.size)
-        self.assertEqual(10, self.bst.root.data)
-        self.show([('size', self.bst.size), ('root value', self.bst.root.data)])
+    def test_insert(self):
+        self.assertEqual(len(self.arr), self.bst.size)
+        self.show('size=' + str(self.bst.size))
 
-    def test_insert_left(self):
-        self.bst.insert(10)
-        self.bst.insert(5)
-        self.assertEqual(2, self.bst.size)
-        self.assertEqual(10, self.bst.root.data)
-        self.assertEqual(5, self.bst.root.left.data)
-        self.show(
-            [('size', self.bst.size), ('root value', self.bst.root.data), ('left child', self.bst.root.left.data)])
+    def test_search(self):
+        data = 0
+        node, parent = self.bst.search(data)
+        self.assertEqual(data, node.data)
+        self.assertIsNotNone(parent)
+        self.show(str(data) + '==' + str(node.data))
 
-    def test_insert_right(self):
-        self.bst.insert(10)
-        self.bst.insert(19)
-        self.assertEqual(2, self.bst.size)
-        self.assertEqual(10, self.bst.root.data)
-        self.assertEqual(19, self.bst.root.right.data)
-        self.show(
-            [('size', self.bst.size), ('root value', self.bst.root.data), ('right child', self.bst.root.right.data)])
+    def test_find_min(self):
+        result = self.bst.find_min(self.bst.root)
+        self.assertEqual(min(self.arr), result.data)
+        self.show(result.data)
 
-    def test_search_existing_item(self):
-        self.populate()
-        node = self.bst.search(6)
-        self.show(self.bst.in_order())
-        self.assertIsNotNone(node)
-        self.assertEqual(6, node.data)
-        # self.show('Found!')
-
-    def test_search_none_existing_item(self):
-        self.populate()
-        node = self.bst.search(100)
-        self.assertIsNone(node)
-        self.show('Not Found!')
-
-    def test_remove_existing_item(self):
-        self.populate()
-        size = self.bst.size
-        expected_size = size - 1
-        self.bst.remove(5)
-        result = self.bst.search(5)
-        self.assertIsNotNone(result)
-        self.assertEqual(expected_size, size)
-
-    def test_remove_none_existing_item(self):
-        self.populate()
-        with self.assertRaises(Exception):
-            self.bst.remove(5)
-
-    def test_pre_order(self):
-        self.populate()
-        result = self.bst.pre_order()
-        self.show(result)
-
-    def test_post_order(self):
-        self.populate()
-        result = self.bst.post_order()
-        self.show(result)
+    def test_find_max(self):
+        result = self.bst.find_max(self.bst.root)
+        self.assertEqual(max(self.arr), result.data)
+        self.show(result.data)
 
     def test_in_order(self):
-        self.populate()
         result = self.bst.in_order()
+        self.arr.sort()
+        self.assertEqual(self.arr, result)
         self.show(result)
 
-    def populate(self):
-        for i in [10, 5, 19, 1, 6, 17]:
-            self.bst.insert(i)
+    def test_remove_node_with_no_leaves(self):
+        self.bst.remove(0)
+        self.arr.sort()
+        self.assertEqual(len(self.arr)-1, self.bst.size)
+        self.arr.remove(0)
+        self.assertEqual(self.arr, self.bst.in_order())
+        self.show(self.bst.in_order)
+
+    def test_remove_node_one_left_leaf(self):
+        self.bst.remove(1)
+        self.arr.sort()
+        self.assertEqual(len(self.arr) - 1, self.bst.size)
+        self.arr.remove(1)
+        self.assertEqual(self.arr, self.bst.in_order())
+        self.show(self.bst.in_order)
