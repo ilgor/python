5be67f690d5585075fc956a2569e70f58290f6b7
ilgor <ilgorbek@gmail.com>
2017-05-08 17:19:36 -0500

    fibonacci added

---
+++ DynamicProgramming/Fibonacci/fibonacci.py
@@ -0,0 +1,25 @@
+
+
+class Fibonacci():
+    def __init__(self):
+        self.dict = {}
+
+    def get_nth_fibonacci_number(self, n):
+        if n < 1:
+            raise Exception("Can't generate less than 1 element. Please enter at least 1.")
+        elif n == 1:
+            return 0
+        elif n == 2:
+            return 1
+
+        if n in self.dict:
+            return self.dict.get(n)
+        new_val = self.get_nth_fibonacci_number(n-1) + self.get_nth_fibonacci_number(n-2)
+        self.dict[n] = new_val
+        return new_val
+
+    def generate_fibonacci_sequence_for(self, n):
+        arr = []
+        for i in range(1, n+1):
+            arr.append(self.get_nth_fibonacci_number(i))
+        return arr
---
+++ DynamicProgramming/Fibonacci/fibonacci_test.py
@@ -0,0 +1,34 @@
+import unittest
+import inspect
+
+from fibonacci import Fibonacci
+
+class fibonacci_test(unittest.TestCase):
+    def setUp(self):
+        self.fib = Fibonacci()
+
+    def show(self, msg):
+        print(inspect.stack()[1][3], msg)
+
+    def test_method(self):
+        self.show("test method")
+
+    def test_get_nth_fibonacci_number_shouldRaiseException(self):
+        n = -1
+        with self.assertRaises(Exception):
+            self.fib.get_nth_fibonacci_number(n)
+        self.show("Exception raised")
+
+    def get_nth_fibonacci_number(self):
+        n = 5
+        expected = 3
+        actual  = self.fib.get_nth_fibonacci_number(n)
+        self.show(actual)
+        self.assertEqual(expected, actual)
+
+    def test_generate_fibonacci_sequence_for(self):
+        n = 5
+        expected = [0,1,1,2,3]
+        actual = self.fib.generate_fibonacci_sequence_for(n)
+        self.show(expected)
+        self.assertEqual(expected, actual)
